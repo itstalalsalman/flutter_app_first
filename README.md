d3701d1df3189e4c5c484dd815967c2fc426104f
Talal Salman Zafeer <mustangcreator@gmail.com>
2024-03-18 01:26:08 +0300

    Sizing issues Fixed, Along with Animations added

--- README.md
+++ README.md
@@ -1,21 +1,45 @@
 
-# Features Implemented
+# New Features Implemented
+
 ## Verification Page
 A whole new verificaion_page.dart class is implemented to design the page on which when the basla button is pressed the navigator takes the user to the orders page.
 
+This can be found under /lib/pages/verification_page.dart 
+
 The verification page design can be found under the path lib/pages/verification_page.dart
 On the main.dart when application runs the verification page is showed.
+
+![Main Page](./lib/images/verification_page.JPG) 
 
 ## Images Added 
 Certain images are added for the logo import from figma to design the page.
 They can be found under lib/images/
 
+
 ## Updated pubsec.yaml 
 The file has been adjusted to add the images and add the fonts from the figma file.
+line 34-35 has the added packages
 
-From line 192 - 221
+## Sizing issue fixed for the mobile devices
+The overflow of pixels issues has been fixed by resizing certain elements
 
-# Image
+## Fade In & Slide from left animation added to the verification page
+Visibility Detector used and animation to slide the text as the first page loads.
+This can be found under /lib/pages/verification_page.dart 
 
-![Verification Page](./lib/images/verification_page.JPG) 
+## On Seach Feature Implemented
+User can filter the search for resturants using the search bar implemented which can be found under 
+
+
+From line 191 - 237
+
+![No Search](./lib/images/no_search.JPG) 
+
+![On Search](./lib/images/on_search.JPG) 
+
+
+
+
+
+
 
--- lib/pages/verification_page.dart
+++ lib/pages/verification_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'order_page.dart'; // Import OrderPage if it's in a separate file
+import 'package:visibility_detector/visibility_detector.dart';
 
 class VerificationPage extends StatefulWidget {
   const VerificationPage({super.key});
@@ -8,11 +9,36 @@ class VerificationPage extends StatefulWidget {
   State<VerificationPage> createState() => _VerificationPageState();
 }
 
-class _VerificationPageState extends State<VerificationPage> {
+class _VerificationPageState extends State<VerificationPage> with SingleTickerProviderStateMixin {
   final phoneNumberController = TextEditingController();
 
   void goToOrdersPage() {
     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => OrdersPage()));
+  }
+
+  late AnimationController _animationController;
+  late Animation<Offset> _slideAnimation;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController( 
+      duration: const Duration(milliseconds: 1000),
+      vsync: this, 
+    );
+
+    // Initialize animations here 
+    _slideAnimation = Tween<Offset>(begin: const Offset(-0.5, 0), end: Offset.zero)
+        .animate(_animationController);
+
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -22,34 +48,48 @@ class _VerificationPageState extends State<VerificationPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            const SizedBox(height: 50),
-            Image.asset('lib/images/LOGO.jpg', width: 200,), // Replace with your image paths
-            const SizedBox(height: 50),
-            Image.asset('lib/images/kitchen.png', width: 280,),
+            const SizedBox(height: 35),
+            Image.asset('lib/images/LOGO.jpg', width: 150,), // Replace with your image paths
+            const SizedBox(height: 25),
+            Image.asset('lib/images/kitchen.png', width: 270,),
             const SizedBox(height: 20),
-            const Padding(
-              padding: EdgeInsets.only(left: 40.0, right: 20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Tencere Aşçı\'ya Hoşgeldin',
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, fontFamily: 'Poppins'),
+            VisibilityDetector(
+              key: UniqueKey(), // Unique key to trigger animation correctly
+              onVisibilityChanged: (visibilityInfo) {
+                if (visibilityInfo.visibleFraction > 0.2) { // Trigger when 20% visible
+                  _animationController.forward();
+                }
+              },
+              child: SlideTransition( 
+                position: _slideAnimation,
+                child: FadeTransition(
+                  opacity: _fadeAnimation,
+                  child: const Padding(
+                    padding: EdgeInsets.only(left: 30.0, right: 10.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Tencere Aşçı\'ya Hoşgeldin',
+                          style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, fontFamily: 'Poppins'),
+                        ),
+                        Text(
+                          'Kayıt ol ve Sipariş Al!',
+                          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque hendrerit, enim non bibendum cursus, nunc loremtristique leo, quis fermentum tortor arcu non ipsum.'
+                        , style: TextStyle(fontSize: 12, fontFamily: 'Poppins', height: 2,),
+                        textAlign: TextAlign.left,),
+                      ],
+                    ),
                   ),
-                  Text(
-                    'Kayıt ol ve Sipariş Al!',
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque hendrerit, enim non bibendum cursus, nunc loremtristique leo, quis fermentum tortor arcu non ipsum.'
-                  , style: TextStyle(fontSize: 11, fontFamily: 'Poppins', height: 2,),
-                  textAlign: TextAlign.left,),
-                ],
+                ),
               ),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 15),
             ElevatedButton(
               onPressed: goToOrdersPage,
               style: ElevatedButton.styleFrom(
